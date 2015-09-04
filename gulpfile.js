fdb983a7a64184099bd8637dfc39dbffe8b96f20
alexballera <alexballera@gmail.com>
2015-09-04 01:01:28 -0430

    Configurado variables globales

--- gulpfile.js
+++ gulpfile.js
@@ -12,14 +12,28 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     del = require('del');
 
+var globs = {
+  sass: 'app/styles/sass/styles.scss',
+  css: 'app/styles/css/styles.css',
+  js: 'app/scripts/main.js',
+  html: 'app/index.html',
+  image: 'app/assets/images/*',
+  folder: [
+    'app/styles/css',
+    'app/scripts/js',
+    'app/images'
+  ]
+}
+
 // Server
+var port = 4000;
 gulp.task('express', function() {
   var express = require('express');
   var app = express();
   app.use(require('connect-livereload')({port: 35729}));
   app.use(express.static(__dirname + '/app'));
-  app.listen(4000, '0.0.0.0');
-  console.log('Saliendo en http://localhost:4000');
+  app.listen(port, '0.0.0.0');
+  console.log('Saliendo en http://localhost:' + port);
 });
 
 // Livereload & Notify
@@ -40,50 +54,50 @@ function notifyLiveReload(event) {
 }
 // Styles
 gulp.task('styles', function() {
-  return sass('app/styles/sass/styles.scss', { style: 'expanded' })
+  return sass(globs.sass, { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('app/styles/css'))
+    .pipe(gulp.dest(globs.folder[0]))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
-    .pipe(gulp.dest('app/styles/css'))
+    .pipe(gulp.dest(globs.folder[0]))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
 // Scripts
 gulp.task('scripts', function() {
-  return gulp.src('app/scripts/main.js')
+  return gulp.src(globs.js)
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('app/scripts/js'))
+    .pipe(gulp.dest(globs.folder[1]))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest('app/scripts/js'))
+    .pipe(gulp.dest(globs.folder[1]))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // Images
 gulp.task('images', function() {
-  return gulp.src('app/assets/images/*')
+  return gulp.src(globs.image)
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('app/images'))
+    .pipe(gulp.dest(globs.folder[2]))
     .pipe(notify({ message: 'Images task complete' }));
 });
 
 // Clean
 gulp.task('clean', function(cb) {
-    del(['app/styles/css/styles.css', 'app/scripts/js/main.js'], cb)
+    del([globs.css, globs.folder[1] + '/main.js'], cb)
 });
 
 // Watch
 gulp.task('watch', function() {
-  gulp.watch('app/styles/sass/styles.scss', ['styles']);
-  gulp.watch('app/scripts/main.js', ['scripts']);
-  gulp.watch('app/assets/images/*', ['images']);
-  gulp.watch('app/index.html', notifyLiveReload);
-  gulp.watch('app/styles/css/*', notifyLiveReload);
-  gulp.watch('app/scripts/js/*', notifyLiveReload);
-  gulp.watch('app/images/*', notifyLiveReload);
+  gulp.watch(globs.sass, ['styles']);
+  gulp.watch(globs.js, ['scripts']);
+  gulp.watch(globs.image, ['images']);
+  gulp.watch(globs.html, notifyLiveReload);
+  gulp.watch(globs.folder[0] + '/*', notifyLiveReload);
+  gulp.watch(globs.folder[1] + '/*', notifyLiveReload);
+  gulp.watch(globs.folder[2] + '/*', notifyLiveReload);
 });
 
 // Default task
